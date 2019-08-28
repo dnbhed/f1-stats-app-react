56b59b52fce36a2463c58fc9db2402e00b5cb422
TamHOD <thomashodonnell@gmail.com>
2019-08-28 19:22:46 +0100

    change css

--- src/Containers/NavBar.js
+++ src/Containers/NavBar.js
@@ -6,11 +6,12 @@ import { Link } from 'react-router-dom'
 const NavBar = (props) => {
     return (
         <nav id="nav-bar">
-            <h1 id="driver-stats-link"><Link to="/driver-stats">Driver Stats</Link></h1>
-            <h1 id="compare-drivers-link"><Link to="/compare-drivers">Compare Drivers</Link></h1>
-            <h1 id="logo-home"><Link to="/">F1 V</Link></h1>
-            <h1 id="track-stats-link"><Link to="/track-stats">Track Stats</Link></h1>
-            <h1 id="constructor-stats-link"><Link to="/constructor-stats">Constructor Stats</Link></h1>
+            <h1 id="driver-stats-link"><Link to="/driver-stats" style={{ textDecoration: 'none' }}>Driver Stats</Link></h1>
+            <h1 id="logo-home" style={{ textDecoration: 'none' }}><Link to="/">F1 V</Link></h1>
+            <h1 id="compare-drivers-link"><Link to="/compare-drivers" style={{ textDecoration: 'none' }}>Compare Drivers</Link></h1>
+
+            {/* <h1 id="track-stats-link" style={{ textDecoration: 'none' }}><Link to="/track-stats">Track Stats</Link></h1>
+            <h1 id="constructor-stats-link" style={{ textDecoration: 'none' }}><Link to="/constructor-stats">Constructor Stats</Link></h1> */}
         </nav>
     )
 }
