09e29ce51234d8d96a4de0648921449b32f55079
TamHOD <thomashodonnell@gmail.com>
2019-10-15 13:25:41 +0100

    prettier some code

--- src/Components/SelectComponents/TrackSelect.js
+++ src/Components/SelectComponents/TrackSelect.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
-import './DriverSelect.css'
+import "./DriverSelect.css";
 
-const TrackSelect = (props) => {
-
-    const options = props.tracks.map((track, index) => {
-        return <Dropdown.Item as="button" eventKey={index} value={track.circuitId} onClick={handleChange}>
-    {track.Location.country}: {track.circuitName}
-  </Dropdown.Item>;
-    })
-
-    function handleChange(event) {
-        props.onTrackSelected(event)
-    }
-
+const TrackSelect = props => {
+  const options = props.tracks.map((track, index) => {
     return (
-        
-        <Dropdown>
-        <Dropdown.Toggle variant="success" id="track-selector" >
-          Select Track
-        </Dropdown.Toggle>
-  
-        <Dropdown.Menu>{options}</Dropdown.Menu>
-      </Dropdown>
-    )
-}
+      <Dropdown.Item
+        as="button"
+        eventKey={index}
+        value={track.circuitId}
+        onClick={handleChange}
+      >
+        {track.Location.country}: {track.circuitName}
+      </Dropdown.Item>
+    );
+  });
+
+  function handleChange(event) {
+    props.onTrackSelected(event);
+  }
+
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="track-selector">
+        Select Track
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>{options}</Dropdown.Menu>
+    </Dropdown>
+  );
+};
 
 export default TrackSelect;
